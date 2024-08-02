b10ec801976b3e24b3127b68dcaa560f5091c4a5
yeswanth <yeswanthb40@gmail.com>
2024-08-02 21:09:36 +0530

    updated

---
+++ resources-local-backend/main.tf
@@ -0,0 +1,13 @@
+## Terraform version
+
+terraform {
+  required_version = "1.4.2"
+}
+
+## local backend
+
+terraform {
+  backend "local" {
+    path = "../statefile/terraform.tfstate"
+  }
+}
---
+++ resources-local-backend/provider.tf
@@ -0,0 +1,7 @@
+## Microsoft Azure - provider
+
+provider "azurerm" {
+  version         = ">= 3.35.0"
+  # subscription_id = var.subscription_id
+  features {}
+}
