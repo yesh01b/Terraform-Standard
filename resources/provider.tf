8852f30053dd83099703404c42257cc09425391c
yeswanth <yeswanthb40@gmail.com>
2024-08-02 20:38:17 +0530

    updated

--- resources/provider.tf
+++ resources/provider.tf
@@ -1,7 +1,7 @@
-# ## Microsoft Azure - provider
+## Microsoft Azure - provider
 
-# provider "azurerm" {
-#   version         = ">= 3.35.0"
-#   subscription_id = var.subscription_id
-#   features {}
-# }
+provider "azurerm" {
+  version         = ">= 3.35.0"
+  subscription_id = var.subscription_id
+  features {}
+}
