56cb57e24d4fc393c7f7d3ffa4bde5f428b1c663
yeswanth <yeswanthb40@gmail.com>
2024-08-02 20:42:03 +0530

    updated

--- resources/provider.tf
+++ resources/provider.tf
@@ -2,6 +2,6 @@
 
 provider "azurerm" {
   version         = ">= 3.35.0"
-  subscription_id = var.subscription_id
+  # subscription_id = var.subscription_id
   features {}
 }
--- resources/variables.tf
+++ resources/variables.tf
@@ -1,10 +1,10 @@
 ## General variables
 
-variable "subscription_id" {
-  type        = string
-  description = "The subscription ID where we are deploying the resources"
-  default     = "c4c44e31-c0c8-401b-824e-3fba6c4e6b68"
-}
+# variable "subscription_id" {
+#   type        = string
+#   description = "The subscription ID where we are deploying the resources"
+#   default     = "c4c44e31-c0c8-401b-824e-3fba6c4e6b68"
+# }
 variable "rg_name" {
   type = string
   description = "Resource group name where we are deploying the resources into"
