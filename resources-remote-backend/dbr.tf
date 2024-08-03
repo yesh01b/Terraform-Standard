34e9d7ef9e16ddad286d427634d4e0737942104e
yeswanth <yeswanthb40@gmail.com>
2024-08-03 16:20:53 +0530

    updated

--- resources-remote-backend/dbr.tf
+++ resources-remote-backend/dbr.tf
@@ -3,7 +3,7 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "haschicorp/azurerm"
+        source = "hashicorp/azurerm"
         version = ">= 2.26"
     }
     databricks = {
