194c0b8341c5232709b5204f5864a75a2f34375f
yeswanth <yeswanthb40@gmail.com>
2024-08-03 16:00:11 +0530

    updated

--- resources-remote-backend/dbr.tf
+++ resources-remote-backend/dbr.tf
@@ -1,20 +1,20 @@
-## Terraform - Azure version - DBR 
+# ## Terraform - Azure version - DBR 
 
-terraform {
-  required_providers {
-    azurerm = {
-        source = "haschicorp/azurerm"
-        version = ">= 2.26"
-    }
-    databricks = {
-        source = "databricks/databricks"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#         source = "haschicorp/azurerm"
+#         version = ">= 2.26"
+#     }
+#     databricks = {
+#         source = "databricks/databricks"
+#     }
+#   }
+# }
 
-## NSG association for DBR private Subnet
+# ## NSG association for DBR private Subnet
 
-resource "azurerm_subnet_network_security_group_association" "dbr_public_snet" {
-  subnet_id = azurerm_subnet.snet_dbr_public.id
-  network_security_group_id = azurerm_network_security_group.nsg_dbr_public.id
-}
+# resource "azurerm_subnet_network_security_group_association" "dbr_public_snet" {
+#   subnet_id = azurerm_subnet.snet_dbr_public.id
+#   network_security_group_id = azurerm_network_security_group.nsg_dbr_public.id
+# }
