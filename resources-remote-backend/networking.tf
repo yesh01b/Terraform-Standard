52c57fea5be521c7922f3f7926dee3674dc96b36
yeswanth <yeswanthb40@gmail.com>
2024-08-03 14:39:14 +0530

    Added subnets for the DBR

--- resources-remote-backend/networking.tf
+++ resources-remote-backend/networking.tf
@@ -13,3 +13,18 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg_vnet.name
   address_space       = var.address_space
 }
+
+## Subnet - Databricks - public
+
+resource "azurerm_subnet" "snet_dbr_public" {
+  name                 = var.subnet_dbr_public_name
+  resource_group_name  = azurerm_resource_group.rg_vnet.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.dbr_public_address_prefixes
+}
+resource "azurerm_subnet" "snet_dbr_private" {
+  name                 = var.subnet_dbr_private_name
+  resource_group_name  = azurerm_resource_group.rg_vnet.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.dbr_private_address_prefixes
+}
--- resources-remote-backend/variables.tf
+++ resources-remote-backend/variables.tf
@@ -32,3 +32,21 @@ variable "address_space" {
   type = list(string)
   default = [ "10.1.1.0/24" ]
 }
+variable "subnet_dbr_public_name" {
+  type = string
+  description = "Public subnet for the DBR"
+  default = "snet-dbr-public-dev-01"
+}
+variable "dbr_public_address_prefixes" {
+  type = list(string)
+  default = [ "10.1.1.10/26" ]
+}
+variable "subnet_dbr_private_name" {
+  type = string
+  description = "Private subnet for the DBR"
+  default = "snet-dbr-private-dev-01"
+}
+variable "dbr_private_address_prefixes" {
+  type = list(string)
+  default = [ "10.1.1.20/26" ]
+}
