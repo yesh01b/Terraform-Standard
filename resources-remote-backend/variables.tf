5b8a35b0ce4fad8a2416e8232861ca699082b476
yeswanth <yeswanthb40@gmail.com>
2024-08-03 13:16:06 +0530

    updated

--- resources-remote-backend/variables.tf
+++ resources-remote-backend/variables.tf
@@ -3,12 +3,15 @@
 variable "subscription_id" {
   type        = string
   description = "The subscription ID where we are deploying the resources"
+  default = "c4c44e31-c0c8-401b-824e-3fba6c4e6b68"
 }
 variable "rg_name" {
   type = string
   description = "Resource group name where we are deploying the resources into"
+  default = "rg-dev-01"
 }
 variable "location" {
   type = string
   description = "The location where the resource is deploying"
+  default = "uksouth"
 }
